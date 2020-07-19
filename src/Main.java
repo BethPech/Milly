1df78f2d02b3de9b7964391f04381fa403c54f42
BethPech <colnze17-94@mail.ru>
2020-07-19 20:33:04 +0300

    Bonus added

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 5_373_768;
+        int amount = 1_200;
+        int bonus = ticket / amount;
+
+        System.out.println(bonus);
+    }
+}
